eaa009484c8f496d882f7cc4ad4583f5adb3334f
Hansell Kopp <hansell.kopp@frontend-gmbh.de>
2021-02-22 18:34:14 +0100

    Add service

---
+++ src/app/services/movies.service.ts
@@ -0,0 +1,39 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { tap, map } from 'rxjs/operators';
+import { NowPlaying, Movie } from 'src/app/interfaces/now-playing'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MoviesService {
+  page=1
+  loading=false
+  baseUrl = 'https://api.themoviedb.org/3'
+  constructor(private http: HttpClient) { }
+
+  get params() { 
+  return {
+    api_key: '0f0a409f0dd5a60c3e77a42d88d8cfd7',
+    language:'en-US',
+    sort_by:'popularity.desc',
+    include_adult:'false',
+    include_video: 'false',
+    page: this.page.toString()
+  }
+  }
+  getNowPlaying(): Observable<Movie[]> {
+    if(this.loading) {
+      return of([])
+    }
+    this.loading=true
+    return this.http.get<NowPlaying>(`${this.baseUrl}/movie/now_playing`,{ params: this.params})
+                    .pipe(
+                      map((resp) => resp.results),
+                      tap(()=> { 
+                      this.page += 1
+                      this.loading=false
+                    }))
+  }
+}
